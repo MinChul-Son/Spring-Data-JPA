13e3df94ee1cae0b31d06978a4064833a80b5b6f
MinChul-Son <smc5236@naver.com>
2021-06-10 20:58:27 +0900

    예제 도메인 모델 : 회원, 팀 도메인 만들고, 테스트
    회원과 팀 도메인을 만들고, 이를 Junit을 통해 테스트를 해보았다.

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,23 +1,42 @@
 package study.datajpa.entity;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 기본 스펙으로 기본 생성자가 필요함(protected로)
+@ToString(of = {"id", "username", "age"}) // 연관 관계인 team은 출력하면 안됨. 무한 루프의 가능성이 존재함.
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id") // 테이블명 + id로 매칭해주는게 젤 좋음
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() { // JPA 기본 스펙으로 기본 생성자가 필요함(protected로)
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id") // 연관관계의 주인
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null){
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) { // 멤버는 팀을 변경가능
+        this.team = team;
+        team.getMembers().add(this); // 확실하게 하려면 기존에 팀 컬렉션도 찾아서 삭제해줘야함. 하지만 Team.members는 연관관계의 주인이 아님, 따라서 동작에 영향을 주지 않음.
     }
 }
