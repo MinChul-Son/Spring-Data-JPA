5a6058d2d653bdc883bd36d35175b12ed373684b
MinChul-Son <smc5236@naver.com>
2021-06-10 21:43:09 +0900

    공통 인터페이스 기능 : 순수 JPA 사용하여 만들기
    스프링 데이터 JPA가 제공하는 기능말고, 순수 JPA 를 사용하여 레포지토리를
    만들고 테스트 하였다.

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -17,7 +19,26 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class) // JPQL은 엔티티를 대상으로함
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member); // null값일 수도 있으므로 Optional로 감싸서 반환
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class) // count는 Long타입으로 반환됨
+                .getSingleResult(); // 단건 조회
     }
 }
