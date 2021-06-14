e0b100430c0bdc5991715c37cfbd8c0ee5124258
MinChul-Son <smc5236@naver.com>
2021-06-14 17:45:07 +0900

    쿼리 메서드 기능 : EntityGraph
    JPA의 지연로딩에서 발생하는 N+1 문제를 fetch join을 사용하여 한방쿼리로
    해결한다. EntityGraph 애노테이션은 fetch join을 편하게 사용할 수 있도록
    도와준다.

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -204,4 +204,27 @@ class MemberRepositoryTest {
         assertThat(resultCount).isEqualTo(3);
     }
 
+    @Test
+    public void findMemberLazy() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        List<Member> members = memberRepository.findMemberEntityGraph();
+
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.getTeam().getName() = " + member.getTeam().getName());
+        }
+    }
+
 }
